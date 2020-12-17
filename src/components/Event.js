8fe60ac91352d835ba6b81dfd5c4444116daf91b
tomato1125 <naokiyoshida1125@icoud.com>
2020-12-17 19:59:18 +0900

    Update Event.js

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,17 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useContext } from 'react'
 
-import { 
+import {
   ADD_OPERATION_LOG,
   DELETE_EVENT
 } from '../actions'
 import AppContext from '../contexts/AppContext'
-import { timeCurrentIso8601} from '../utils'
+import { timeCurrentIso8601 } from '../utils'
 
-const Event = ({ event}) => {
+const Event = ({ event }) => {
   const { dispatch } = useContext(AppContext)
   const id = event.id
   const handleClickDeleteButton = () => {
-    const result = window.confirm(`イベント(id=${id})を本当に削除してもよいですか？`)
+    const result = window.confirm(`イベント(id=${id})を本当に削除しても良いですか？`)
     if (result) {
       dispatch({ type: DELETE_EVENT, id })
 
@@ -20,7 +20,9 @@ const Event = ({ event}) => {
         description: `イベント(id=${id})を削除しました。`,
         operatedAt: timeCurrentIso8601()
       })
+    }
   }
+
   return (
     <tr>
       <td>{id}</td>
